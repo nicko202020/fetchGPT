3bc0dbff0b7c645267f6e9ff7a152f4c7f9dba0d
nicK <nicholas.oei@ventitechnologies.com>
2023-12-29 20:04:26 +0800

    succesful intrgration with pygame and autogen

--- v2.py
+++ v2.py
@@ -1,5 +1,6 @@
 import autogen
 import pygame
+import threading 
 
 # Room class definition
 class Room:
@@ -36,12 +37,15 @@ class Robot:
         return "Unknown room"
 # Functions for interacting with the robot
 def move_robot(new_x, new_y):
+    global robot
     return robot.teleport(new_x, new_y)
 
 def get_current_position():
+    global robot
     return robot.current_position()
 
 def get_robot_current_room():
+    global robot
     return robot.current_room()
 
 
@@ -81,18 +85,18 @@ llm_config = {
     "config_list": config_list,
 }
 
-# # Initialize AutoGen agents
-# user = autogen.UserProxyAgent(name="User")
-# robot_agent = autogen.AssistantAgent(name="Robot", llm_config=llm_config)
+# Initialize AutoGen agents
+user = autogen.UserProxyAgent(name="User", human_input_mode="NEVER")
+robot_agent = autogen.AssistantAgent(name="Robot", llm_config=llm_config)
 
-# # Register functions with the UserProxyAgent
-# user.register_function(
-#     function_map={
-#         "move_robot": move_robot,
-#         "get_current_position": get_current_position,
-#         "get_robot_current_room": get_robot_current_room
-#     }
-# )
+# Register functions with the UserProxyAgent
+user.register_function(
+    function_map={
+        "move_robot": move_robot,
+        "get_current_position": get_current_position,
+        "get_robot_current_room": get_robot_current_room
+    }
+)
 
 # # Example AutoGen interaction (hypothetical usage)
 # user.initiate_chat(
@@ -145,21 +149,88 @@ def draw_room(room):
     # Draw the room with a contrasting color
     pygame.draw.rect(screen, (0, 0, 255), [room.x1, room.y1, room.x2 - room.x1, room.y2 - room.y1], 1)
 
-# Main loop
+def handle_command_input(command):
+    def llm_thread():
+        # Initiating chat with the AutoGen agent using the provided command
+        response = user.initiate_chat(
+            robot_agent,
+            message=f"Move robot to position {command}"
+        )
+        # Process the response to update the robot's state
+        # Implement the logic based on how your response is structured
+
+    # Start the LLM communication in a separate thread
+    threading.Thread(target=llm_thread).start()
+def draw_button():
+    pygame.draw.rect(screen, button_color, button)  # Draw the button
+    button_text = font.render('Send', True, WHITE)
+    screen.blit(button_text, (button.x + 20, button.y + 5))
+
+# Input box and button setup
+input_box = pygame.Rect(100, SCREEN_HEIGHT - 40, 140, 32)
+button = pygame.Rect(250, SCREEN_HEIGHT - 40, 80, 32)  # Button position and size
+button_color = pygame.Color('grey12')  # Button color
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
+color = color_inactive
+active = False
+text = ''
+# Main Pygame loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Check if the input box is clicked
+            if input_box.collidepoint(event.pos):
+                active = not active
+            elif button.collidepoint(event.pos):
+                # Handle the button click
+                handle_command_input(text)
+                text = ''  # Clear the text after sending the command
+                active = False  # Deactivate the input box
+            else:
+                active = False
+            color = color_active if active else color_inactive
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    handle_command_input(text)
+                    text = ''  # Clear the text after sending the command
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
 
-    screen.fill((255, 255, 255))  # Use a clearly visible color for the background
+    screen.fill(WHITE)  # Fill the screen with white
 
-    # Draw the room and robot at visible coordinates
+    # Draw the room and robot
     draw_room(living_room)
     robot_position = robot.current_position()
-    pygame.draw.circle(screen, (255, 0, 0), robot_position, 10)
+    pygame.draw.circle(screen, RED, robot_position, 10)
 
+    # Draw the dashboard and input box
     draw_dashboard()
+
+    # Render the current text
+    txt_surface = font.render(text, True, color)
+    width = max(200, txt_surface.get_width() + 10)
+    input_box.w = width
+    screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+    pygame.draw.rect(screen, color, input_box, 2)
+
+    # Draw the button
+    # draw_button()  # Make sure this function is defined to draw the button
+
     pygame.display.flip()
 
 pygame.quit()
+
+
+# Robot initialization
+center_x = SCREEN_WIDTH // 2
+center_y = SCREEN_HEIGHT // 2
+living_room = Room("Living Room", center_x - 100 // 2, center_y - 100 // 2, 
+                   center_x + 100 // 2, center_y + 100 // 2)
+robot = Robot(center_x, center_y, living_room)
